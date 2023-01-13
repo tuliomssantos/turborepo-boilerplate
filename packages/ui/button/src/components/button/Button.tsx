865b24003a53fc79c3a7d13197c817701b5091a8
Tulio <tuliomssantos@gmail.com>
2023-01-13 07:51:32 -0300

    style: button folder structure

--- packages/ui/button/src/components/button/Button.tsx
+++ packages/ui/button/src/components/button/Button.tsx
@@ -4,9 +4,32 @@ import { forwardRef } from 'react'
 
 import objStr from 'obj-str'
 
-import { ButtonProps } from './types'
-
 import styles from './styles.module.scss'
+
+export type ButtonHierarchy = 'primary' | 'secondary' | 'neutral'
+
+export type ButtonSize = 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+
+export type ButtonBorderRadius = 'sm' | 'md' | 'lg' | 'xl' | '2xl'
+
+export type ButtonVariant = 'filled' | 'outlined'
+
+export type ButtonDestructive = boolean
+
+export type BaseButtonProps = {
+  buttonBorderRadius?: ButtonBorderRadius
+  destructive?: ButtonDestructive
+  disabled?: boolean
+  hierarchy?: ButtonHierarchy
+  size?: ButtonSize
+  variant?: ButtonVariant
+}
+
+export interface ButtonProps
+  extends React.ComponentPropsWithRef<'button'>,
+    BaseButtonProps {
+  children: React.ReactNode
+}
 
 /**
  * Button component
